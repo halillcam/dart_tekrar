5d26d1cd109f7076c32bf4115b48a7d67add7986
Halil Ibrahim Cam <halil.cam0020@gmail.com>
2024-11-19 20:13:04 +0300

    Kalıtımda düzeltmeler ve geliştirmeler yapildi

--- oop/inhteritence.dart
+++ oop/inhteritence.dart
@@ -1,35 +1,47 @@
-//Kalitim
+// Kalıtım
 
-class Inhteritence {
-  String marka,model;
-  int teker,hp,kapi;
+class Inheritance {
+  String marka, model;
+  int teker, hp, kapi;
 
-  Inhteritence({required this.marka,required this.model,required this.teker, required this.hp, required this.kapi}){
+  Inheritance({
+    required this.marka,
+    required this.model,
+    required this.teker,
+    required this.hp,
+    required this.kapi,
+  });
 
-    void AracBilgileri(){
-      print("------------ Arac Bilgileri --------------");
-      print("Marka : $marka, Model : $model, Kac Beygir : $hp , Teker sayisi : $teker, Kapi sayisi :  $kapi");
-    }
-    AracBilgileri();
+  void AracBilgileri() {
+    print("------------ Araç Bilgileri --------------");
+    print("Marka : $marka, Model : $model, Kaç Beygir : $hp , Teker Sayısı : $teker, Kapı Sayısı : $kapi");
   }
-
 }
-class bmw extends Inhteritence{
+
+class BMW extends Inheritance {
   bool supermode;
 
-  bmw({required String marka,required String model, required int hp , required int teker, required int kapi, required this.supermode}) 
-  : super (marka: marka,model: model,hp: hp,teker: teker,kapi: kapi){
+  BMW({
+    required String marka,
+    required String model,
+    required int hp,
+    required int teker,
+    required int kapi,
+    required this.supermode,
+  }) : super(marka: marka, model: model, hp: hp, teker: teker, kapi: kapi);
 
-    if(supermode){
-      print("Arac super mode a sahip");
-    }
-    else{
-      print("bu aracta super mode yok");
+  @override
+  void AracBilgileri() {
+    super.AracBilgileri();
+    if (supermode) {
+      print("Araç süper moda sahip.");
+    } else {
+      print("Bu araçta süper mod yok.");
     }
   }
-
 }
+
 void main(List<String> args) {
-  bmw m5 = bmw(marka: "bmw", model: "2024", hp: 170, teker: 4, kapi: 4, supermode: true);
-  m5;
+  BMW m5 = BMW(marka: "BMW", model: "M5 2024", hp: 170, teker: 4, kapi: 4, supermode: true);
+  m5.AracBilgileri();
 }
