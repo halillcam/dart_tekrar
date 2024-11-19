7907867d13f8383ff6a13c3ab668ae025f0daf44
Halil Ibrahim Cam <halil.cam0020@gmail.com>
2024-11-19 20:28:56 +0300

    Polymorphism revize edildi

--- oop/polymorphism.dart
+++ oop/polymorphism.dart
@@ -5,45 +5,45 @@
 */
 
 class Hayvan {
-  void SesCikar() {
-    print("Hayvanın bir sesi var.");
+  void SesCikar(String ses) {
+    print("Hayvanın sesi: $ses");
   }
 
-  void kacAyak() {
-    print("Bu hayvanın kaç ayağı olduğu belirtilmemiş.");
+  void kacAyak(int ayakSayisi) {
+    print("Bu hayvan $ayakSayisi ayaklı.");
   }
 }
 
 class Kedi extends Hayvan {
   @override
-  void SesCikar() {
-    print("Kedinin sesi: Miyav");
+  void SesCikar(String ses) {
+    print("Kedinin sesi: $ses");
   }
 
   @override
-  void kacAyak() {
-    print("Kediler genelde 4 ayaklıdır.");
+  void kacAyak(int ayakSayisi) {
+    print("Kediler genelde $ayakSayisi ayaklıdır.");
   }
 }
 
 class Kus extends Hayvan {
   @override
-  void SesCikar() {
-    print("Kuşun sesi: Cik cik");
+  void SesCikar(String ses) {
+    print("Kuşun sesi: $ses");
   }
 
   @override
-  void kacAyak() {
-    print("Kuşlar genelde 2 ayaklıdır ve uçabilirler.");
+  void kacAyak(int ayakSayisi) {
+    print("Kuşlar genelde $ayakSayisi ayaklıdır ve uçabilirler.");
   }
 }
 
 void main(List<String> args) {
-  Hayvan pusuk = Kedi(); // Polimorfizm burada devrede!
-  pusuk.SesCikar(); // Çıktı: Kedinin sesi: Miyav
-  pusuk.kacAyak();  // Çıktı: Kediler genelde 4 ayaklıdır.
+  Kedi pusuk = Kedi();
+  pusuk.SesCikar("Miyav");
+  pusuk.kacAyak(4);
 
-  Hayvan fenerbahce = Kus();
-  fenerbahce.SesCikar(); // Çıktı: Kuşun sesi: Cik cik
-  fenerbahce.kacAyak();  // Çıktı: Kuşlar genelde 2 ayaklıdır ve uçabilirler.
+  Kus fenerbahce = Kus();
+  fenerbahce.SesCikar("Cik cik cik, kaç lo Galatasaray geldi");
+  fenerbahce.kacAyak(2);
 }
