149ce29267de3b15f1c0abbc6928b005e345e0fe
Halil Ibrahim Cam <halil.cam0020@gmail.com>
2024-11-22 21:00:01 +0300

    Error Handling (hata yönetimi konusunu tekrar edildi)

---
+++ oop/error_handling.dart
@@ -0,0 +1,14 @@
+void main() {
+  int hesapBakiyesi = 1000;
+
+  try {
+    int cekilecekMiktar = 1500;
+    if (cekilecekMiktar > hesapBakiyesi) {
+      throw "Yetersiz bakiye! Hesabinizdaki para = $hesapBakiyesi TL Çekmek istediğiniz miktar: $cekilecekMiktar TL";
+    }
+    hesapBakiyesi -= cekilecekMiktar;
+    print("Para çekildi! Kalan bakiye: $hesapBakiyesi TL");
+  } catch (e) {
+    print("Bir hata oluştu: $e");
+  }
+}
