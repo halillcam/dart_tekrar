9b0ae82b4c4732ea208adbee4d21dde43e0d4648
Halil Ibrahim Cam <halil.cam0020@gmail.com>
2024-11-29 21:25:07 +0300

    Input alma eklendi

---
+++ dongu_test.dart
@@ -0,0 +1,15 @@
+void main(List<String> args) {
+  
+  int degisken = 1;
+
+  while(degisken == 1){
+    print("hi");
+  degisken ++;
+    if(degisken == 3){
+      break;
+      
+    }
+     print("degisken durdu degeri 3 ");
+
+  }
+}
